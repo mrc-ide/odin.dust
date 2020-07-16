58b2685bf3ab1716b2fb05b40d4fd8ddd09f8e17
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-07-16 14:12:00 +0100

    Fix mocks

--- tests/testthat/test-odin-dust.R
+++ tests/testthat/test-odin-dust.R
@@ -276,7 +276,7 @@ test_that("NSE interface can accept a symbol and resolve to value", {
   mockery::expect_called(mock_target, 1)
   expect_equal(
     mockery::mock_args(mock_target)[[1]],
-    list(path, NULL, NULL, NULL))
+    list(path, NULL, NULL, NULL, NULL))
 })
 
 
@@ -289,7 +289,7 @@ test_that("NSE interface can accept a character vector", {
   mockery::expect_called(mock_target, 1)
   expect_equal(
     mockery::mock_args(mock_target)[[1]],
-    list(c("a", "b", "c"), NULL, NULL, NULL))
+    list(c("a", "b", "c"), NULL, NULL, NULL, NULL))
 })
 
 
