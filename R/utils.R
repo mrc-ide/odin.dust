eaaebb54d69ba3521f7e8641512cb7011043364e
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2020-11-09 10:18:01 +0000

    Remove unused utility

--- R/utils.R
+++ R/utils.R
@@ -11,10 +11,6 @@ viapply <- function(x, fun, ...) {
   vapply(x, fun, integer(1), ...)
 }
 
-vnapply <- function(x, fun, ...) {
-  vapply(x, fun, numeric(1), ...)
-}
-
 vcapply <- function(x, fun, ...) {
   vapply(x, fun, character(1), ...)
 }
