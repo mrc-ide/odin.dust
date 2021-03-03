0929e1f2f096376e2a9af0ab294a64a0282327de
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-03-03 18:35:56 +0000

    Exclude the sirs file from lintr

--- inst/examples/sirs.R
+++ inst/examples/sirs.R
@@ -1,3 +1,4 @@
+### nolint start
 ## Definition of the time-step and output as "time"
 dt <- user(1)
 initial(time) <- 0
@@ -51,3 +52,4 @@ dim(p_SI) <- N_age
 dim(m) <- c(N_age, N_age)
 dim(s_ij) <- c(N_age, N_age)
 dim(lambda) <- N_age
+### nolint end
