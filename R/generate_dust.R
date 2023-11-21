d894fd57350050e8844ea3d519879321090a8530
Wes Hinsley <w.hinsley@imperial.ac.uk>
2023-11-21 15:49:44 +0000

    Use library.hpp for %/%

--- R/generate_dust.R
+++ R/generate_dust.R
@@ -56,7 +56,7 @@ generate_dust <- function(ir, options) {
     }
   }
 
-  if (any(c("%%", "min", "max", "sign") %in% used)) {
+  if (any(c("%/%", "%%", "min", "max", "sign") %in% used)) {
     lib <- list(name = "library",
                 declaration = readLines(odin_dust_file("library.hpp")),
                 definition = NULL)
